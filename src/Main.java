182729cf114670ea2c5844146eb6c4bbc1c0c153
Arian <arianlavie@outlook.com>
2021-08-03 11:37:25 +0430

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import ir.Java.Tutorial.Start;
+
+public class Main {
+    public static void main(String Args[]){
+        System.out.println("Hello github");
+        Start st = new Start();
+        st.starter();
+    }
+}
---
+++ src/ir/Java/Tutorial/Start.java
@@ -0,0 +1,14 @@
+package ir.Java.Tutorial;
+
+public class Start {
+    public void starter(){
+
+        int a = 22;
+        int b = 32;
+        short c = 5;
+        long d = 10000;
+        float e = 22.4f;
+        double f = 34.2;
+        System.out.println(a+b+c+d+e+f);
+    }
+}
